0b0757bede0c36c7ebf9602bda8138a95fd299ea
Michel Reis <michelreisdev@gmail.com.br>
2022-06-06 16:57:18 -0300

    V1

--- src/database/migrations/20220606192852-alunos.js
+++ src/database/migrations/20220606192852-alunos.js
@@ -1,21 +1,50 @@
-'use strict';
-
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+
+
+    return queryInterface.createTable('alunos', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      peso: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      altura: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      create_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      update_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+    });
+
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface) {
+
+
+    return queryInterface.dropTable('alunos');
+
   }
 };
